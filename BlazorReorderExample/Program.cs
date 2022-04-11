602f9816cbbc8a45e8635dcf2cdbe8d1e97d3088
Santiago Cattaneo <santiago@rd-its.com>
2022-04-11 18:35:46 -0300

    * created callback functionality * added example page * added example class and service * removed logs

--- BlazorReorderExample/Program.cs
+++ BlazorReorderExample/Program.cs
@@ -8,7 +8,22 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<BlazorReorderList.ReorderService<ListItem>>();
+builder.Services.AddScoped<BlazorReorderList.ReorderService<ItemList>>();
 
 await builder.Build().RunAsync();
 
 public record ListItem(string title, string url, string details);
+
+public class ItemList
+{
+    public string title { get; set; }
+    public string details { get; set; }
+    public string domClass { get; set; }
+
+    public ItemList(string a, string c)
+    {
+        title = a;
+        domClass = "";
+        details = c;
+    }
+}
